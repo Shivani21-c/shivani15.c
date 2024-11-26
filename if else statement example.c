2a0a33b695a76da904646fb8eb48c02a68d46704
Shivani21-c <sonusinchana06@gmail.com>
2024-11-25 23:35:06 -0800

    Create if else statement example.c

---
+++ if else statement example.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() {
+    int age = 17;
+
+    if (age >= 18) {
+        printf("You are an adult.\n");
+    } else {
+        printf("You are not yet an adult.\n");
+    }
+
+    return 0;
+}
