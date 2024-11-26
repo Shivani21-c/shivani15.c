464b842766b79fd4ce1f858e110847bf27d53b72
Shivani21-c <sonusinchana06@gmail.com>
2024-11-25 22:34:29 -0800

    Add files via upload

---
+++ shivani.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+intmain(void)
+{
+  int a, b, sum;
+printf("enter two numbers");
+  scanf("%d %d",&a, &b);
+sum= (a + b);
+printf("sum of two numbers is %d",sum);
+  printf("sinchana.c"
+  return 0;
+}
